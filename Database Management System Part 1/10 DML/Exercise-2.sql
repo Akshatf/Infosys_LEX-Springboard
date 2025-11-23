2b4a4cb2271cf892abe3bba9dac767155c0496d7
Akshatf <akshatfarkya7@gmail.com>
2025-11-24 01:27:30 +0530

    Added DML

--- Database Management System Part 1/10 DML/Exercise-2.sql
+++ Database Management System Part 1/10 DML/Exercise-2.sql
@@ -1,11 +1,11 @@
-Create the Tournament table as per information below:
+-- Create the Tournament table as per information below:
 
-Column Name 	Data Type 		Constraint 		Description
-TId 			INTEGER 		PRIMARY KEY 	Unique tournament Id for every tournament
-TName 		VARCHAR2(30) 	NOT NULL 		Tournament Name
-StartDt 		DATE 			NOT NULL 		Start Date of the tournament
-EndDt 		DATE 			NOT NULL 		End Date of the tournament
-Prize 		INTEGER 		NOT NULL 		Prize to be won in the tournament
+-- Column Name 	Data Type 		Constraint 		Description
+-- TId 			INTEGER 		PRIMARY KEY 	Unique tournament Id for every tournament
+-- TName 		VARCHAR2(30) 	NOT NULL 		Tournament Name
+-- StartDt 		DATE 			NOT NULL 		Start Date of the tournament
+-- EndDt 		DATE 			NOT NULL 		End Date of the tournament
+-- Prize 		INTEGER 		NOT NULL 		Prize to be won in the tournament
 
 
 Query : 
--- Database Management System Part 1/10 DML/Exercise-7-ALTER-TABLE.sql
+++ Database Management System Part 1/10 DML/Exercise-7-ALTER-TABLE.sql
@@ -1,10 +1,10 @@
-Drop Column 'ContactNo' from table Player.
+-- Drop Column 'ContactNo' from table Player.
 
-ColumnName		Data Type		Constraint		Description
-PId			INTEGER		PRIMARY KEY		Unique Player Id
-PName			VARCHAR2(20)	NOT NULL		Player Name
-Ranking		INTEGER					Player's ranking
-ContactNo		NUMBER(10)					Player Contact Number
+-- ColumnName		Data Type		Constraint		Description
+-- PId			INTEGER		PRIMARY KEY		Unique Player Id
+-- PName			VARCHAR2(20)	NOT NULL		Player Name
+-- Ranking		INTEGER					Player's ranking
+-- ContactNo		NUMBER(10)					Player Contact Number
 
 
 Query : ALTER TABLE Player DROP (ContactNo)
